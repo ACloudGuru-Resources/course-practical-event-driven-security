fb7a375a75cd30c68cf97328daed4738d1f6443d
Alexander Higgins <TheAlexanderHiggins@gmail.com>
2019-05-27 20:55:48 -0400

    Added URLs for chapters 2-5

---
+++ chapter-5/chapter-5-3-macie-samples/sample.js
@@ -0,0 +1,20 @@
+// Sample src code from lodash
+import upperFirst from './upperFirst.js'
+
+/**
+ * Converts the first character of `string` to upper case and the remaining
+ * to lower case.
+ *
+ * @since 3.0.0
+ * @category String
+ * @param {string} [string=''] The string to capitalize.
+ * @returns {string} Returns the capitalized string.
+ * @example
+ *
+ * capitalize('FRED')
+ * // => 'Fred'
+ */
+const capitalize = (string) =>  upperFirst(string.toLowerCase())
+
+
+export default capitalize
